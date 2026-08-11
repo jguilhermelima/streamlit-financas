367f83e45fe0d71cfdbc9bdacb9a5536c37874dc
jguilhermelima <j.guilhermelima@outlook.com>
2026-08-11 15:38:30 -0300

    feat: deploy

--- main.py
+++ main.py
@@ -1,5 +1,118 @@
+import datetime
+
 import pandas as pd
+import requests
 import streamlit as st
+
+
+@st.cache_data(ttl="1day")
+def get_selic():
+    url = "https://www.bcb.gov.br/api/servico/sitebcb/historicotaxasjuros"
+    resp = requests.get(url)
+    df = pd.DataFrame(resp.json()["conteudo"])
+    df["DataInicioVigencia"] = pd.to_datetime(df["DataInicioVigencia"]).dt.date
+    df["DataFimVigencia"] = pd.to_datetime(df["DataFimVigencia"]).dt.date
+    df["DataFimVigencia"] = df["DataFimVigencia"].fillna(
+        datetime.datetime.today().date()
+    )
+    return df
+
+
+def cal_general_stats(df):
+    df_data = df.groupby(by="Data")[["Valor"]].sum()
+    df_data["lag_1"] = df_data["Valor"].shift(1)
+    df_data["Diferença Mensal Abs."] = df_data["Valor"] - df_data["lag_1"]
+    df_data["Média 6M diferença Mensal"] = (
+        df_data["Diferença Mensal Abs."].rolling(6).mean()
+    )
+    df_data["Média 12M diferença Mensal"] = (
+        df_data["Diferença Mensal Abs."].rolling(12).mean()
+    )
+    df_data["Média 24M diferença Mensal"] = (
+        df_data["Diferença Mensal Abs."].rolling(24).mean()
+    )
+    df_data["Diferença Mensal Rel."] = df_data["Valor"] / df_data["lag_1"] - 1
+    df_data["Evolução 6M Total"] = (
+        df_data["Valor"].rolling(6).apply(lambda x: x[-1] - x[0])
+    )
+    df_data["Evolução 12M Total"] = (
+        df_data["Valor"].rolling(12).apply(lambda x: x[-1] - x[0])
+    )
+    df_data["Evolução 24M Total"] = (
+        df_data["Valor"].rolling(24).apply(lambda x: x[-1] - x[0])
+    )
+    df_data["Evolução 6M Relativa"] = (
+        df_data["Valor"].rolling(6).apply(lambda x: x[-1] / x[0] - 1)
+    )
+    df_data["Evolução 12M Relativa"] = (
+        df_data["Valor"].rolling(12).apply(lambda x: x[-1] / x[0] - 1)
+    )
+    df_data["Evolução 24M Relativa"] = (
+        df_data["Valor"].rolling(24).apply(lambda x: x[-1] / x[0] - 1)
+    )
+
+    df_data = df_data.drop("lag_1", axis=1)
+    return df_data
+
+
+def main_metas():
+    col1, col2 = st.columns(2)
+
+    data_inicio_meta = col1.date_input("Inicio de Meta", max_value=df_stats.index.max())
+    data_filtrada = df_stats.index[df_stats.index <= data_inicio_meta][-1]
+
+    custos_fixos = col1.number_input("Custos Finais", min_value=0.0, format="%.2f")
+
+    salario_bruto = col2.number_input("Salário Bruto", min_value=0.0, format="%.2f")
+    salario_liq = col2.number_input("Salário Liquido", min_value=0.0, format="%.2f")
+
+    valor_inicio = df_stats.loc[data_filtrada]["Valor"]
+    col1.markdown(f"**Patrimônio no Inicio da Meta** R${valor_inicio:.2f}")
+
+    selic_gov = get_selic()
+    filter_selic_date = (selic_gov["DataInicioVigencia"] < data_inicio_meta) & (
+        selic_gov["DataFimVigencia"] > data_inicio_meta
+    )
+    selic_default = selic_gov[filter_selic_date]["MetaSelic"].iloc[0]
+
+    selic = st.number_input("Selic", min_value=0.0, value=selic_default, format="%.2f")
+    selic_ano = selic / 100
+    selic_mes = (selic_ano + 1) ** (1 / 12) - 1
+
+    rendimento_ano = valor_inicio * selic_ano
+    rendimento_mes = valor_inicio * selic_mes
+
+    col1_pot, col2_pot = st.columns(2)
+    mensal = salario_liq - custos_fixos + rendimento_mes
+    anual = 12 * (salario_liq - custos_fixos) + rendimento_ano
+
+    with col1_pot.container(border=True):
+        st.markdown(
+            f"""**Potencial Arrecadanção Mês** \n\n R${mensal:.2f}""",
+            help=f"{salario_liq:.2f} + (-{custos_fixos:.2f}) + {rendimento_mes:.2f}",
+        )
+
+    with col2_pot.container(border=True):
+        st.markdown(
+            f"""**Potencial Arrecadanção Ano** \n\n R${anual:.2f}""",
+            help=f"12 * ({salario_liq:.2f} + (-{custos_fixos:.2f}) + {rendimento_ano:.2f})",
+        )
+
+    with st.container(border=True):
+        col1_meta, col2_meta = st.columns(2)
+        with col1_meta:
+            meta_estipulada = st.number_input(
+                "Meta Estipulada",
+                min_value=0.0,
+                format="%.2f",
+                value=anual,
+            )
+
+        with col2_meta:
+            patrimonio_final = meta_estipulada + valor_inicio
+            st.markdown(f"Patrimônio Estimado pós meta: \n\n R$ {patrimonio_final:.2f}")
+    return data_inicio_meta, valor_inicio, meta_estipulada, patrimonio_final
+
 
 st.set_page_config(page_title="Finanças", page_icon="💰")
 
@@ -30,9 +143,11 @@ if file_upload:
     # duas maneiras de usar tab uma direta e outra com with
     tab_data.dataframe(df_instituicao)
 
+    # exibir historico
     with tab_history:
         st.line_chart(df_instituicao)
 
+    # exibir distribuição
     with tab_share:
         # # Uma maneira de selecionar data com calendario para plotar grafico
         # date = st.date_input(
@@ -49,3 +164,128 @@ if file_upload:
         # Segunda maneira utilizando um selectbox apenas com as datas do df
         date = st.selectbox("Filtro Data", options=df_instituicao.index)
         st.bar_chart(df_instituicao.loc[date])
+
+    exp3 = st.expander("Estatística Gerais")
+    df_stats = cal_general_stats(df)
+
+    colums_config = {
+        "Valor": st.column_config.NumberColumn("Valor", format="R$ %.2f"),
+        "Diferença Mensal Abs.": st.column_config.NumberColumn(
+            "Diferença Mensal Abs.", format="R$ %.2f"
+        ),
+        "Média 6M diferença Mensal": st.column_config.NumberColumn(
+            "Média 6M diferença Mensal", format="R$ %.2f"
+        ),
+        "Média 12M diferença Mensal": st.column_config.NumberColumn(
+            "Média 12M diferença Mensal", format="R$ %.2f"
+        ),
+        "Média 24M diferença Mensal": st.column_config.NumberColumn(
+            "Média 24M diferença Mensal", format="R$ %.2f"
+        ),
+        "Diferença Mensal Rel.": st.column_config.NumberColumn(
+            "Diferença Mensal Rel.", format="percent"
+        ),
+        "Evolução 6M Total": st.column_config.NumberColumn(
+            "Evolução 6M Total", format="R$ %.2f"
+        ),
+        "Evolução 12M Total": st.column_config.NumberColumn(
+            "Evolução 12M Total", format="R$ %.2f"
+        ),
+        "Evolução 24M Total": st.column_config.NumberColumn(
+            "Evolução 24M Total", format="R$ %.2f"
+        ),
+        "Evolução 6M Relativa": st.column_config.NumberColumn(
+            "Evolução 6M Relativa", format="percent"
+        ),
+        "Evolução 12M Relativa": st.column_config.NumberColumn(
+            "Evolução 12M Relativa", format="percent"
+        ),
+        "Evolução 24M Relativa": st.column_config.NumberColumn(
+            "Evolução 24M Relativa", format="percent"
+        ),
+    }
+
+    tab_stats, tab_abs, tab_rel = exp3.tabs(
+        tabs=["Dados", "Histórico de Evolução", "Crescimento Relativo"]
+    )
+
+    with tab_stats:
+        st.dataframe(df_stats, column_config=colums_config)
+
+    with tab_abs:
+        abs_cols = [
+            "Diferença Mensal Abs.",
+            "Média 6M diferença Mensal",
+            "Média 12M diferença Mensal",
+            "Média 24M diferença Mensal",
+        ]
+
+        st.line_chart(df_stats[abs_cols])
+
+    with tab_rel:
+        rel_cols = [
+            "Diferença Mensal Rel.",
+            "Evolução 6M Relativa",
+            "Evolução 12M Relativa",
+            "Evolução 24M Relativa",
+        ]
+        st.line_chart(data=df_stats[rel_cols])
+
+    with st.expander("Metas"):
+        tab_main, tab_data_meta, tab_graph = st.tabs(
+            tabs=["Configuração", "Dados", "Gráficos"]
+        )
+
+        with tab_main:
+            data_inicio_meta, valor_inicio, meta_estipulada, patrimonio_final = (
+                main_metas()
+            )
+
+        with tab_data_meta:
+            meses = pd.DataFrame(
+                {
+                    "Data Referência": [
+                        data_inicio_meta + pd.DateOffset(months=i) for i in range(1, 13)
+                    ],
+                    "Meta Mensal": [
+                        valor_inicio + round(meta_estipulada / 12, 2) * i
+                        for i in range(1, 13)
+                    ],
+                }
+            )
+            meses["Data Referência"] = meses["Data Referência"].dt.strftime("%Y-%m")
+
+            df_patrimonio = df_stats.reset_index()[["Data", "Valor"]]
+            df_patrimonio["Data Referência"] = pd.to_datetime(
+                df_patrimonio["Data"]
+            ).dt.strftime("%Y-%m")
+            meses = meses.merge(df_patrimonio, how="left", on="Data Referência")
+
+            meses = meses[["Data Referência", "Meta Mensal", "Valor"]]
+            meses["Atingimento ($)"] = meses["Valor"] / meses["Meta Mensal"]
+            meses["Atingimento Ano"] = meses["Valor"] / patrimonio_final
+            meses["Atingimento Esperado"] = meses["Meta Mensal"] / patrimonio_final
+            meses = meses.set_index("Data Referência")
+
+            colums_config_meta = {
+                "Valor": st.column_config.NumberColumn(
+                    "Valor Atingido", format="R$ %.2f"
+                ),
+                "Meta Mensal": st.column_config.NumberColumn(
+                    "Meta Mensal", format="R$ %.2f"
+                ),
+                "Atingimento ($)": st.column_config.NumberColumn(
+                    "Atingimento ($)", format="percent"
+                ),
+                "Atingimento Ano": st.column_config.NumberColumn(
+                    "Atingimento Ano", format="percent"
+                ),
+                "Atingimento Esperado": st.column_config.NumberColumn(
+                    "Atingimento Esperado", format="percent"
+                ),
+            }
+
+            st.dataframe(meses, column_config=colums_config_meta)
+
+        with tab_graph:
+            st.line_chart(meses[["Atingimento Ano", "Atingimento Esperado"]])
